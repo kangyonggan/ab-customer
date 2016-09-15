5d019535eb402dbd62ec430f6b77aa1f13327337
“houbb” <1060732496@qq.com>
2016-09-15 18:42:08 +0800

    工具类

---
+++ customer-common/src/main/java/com/anybuy/customer/seq/Id.java
@@ -0,0 +1,12 @@
+package com.anybuy.customer.seq;
+
+/**
+ * Created by houbinbin on 16/9/9.
+ */
+public interface Id {
+    /**
+     * 获取ID
+     * @return
+     */
+    String getId();
+}
---
+++ customer-common/src/main/java/com/anybuy/customer/util/ArgUtil.java
@@ -0,0 +1,14 @@
+package com.anybuy.customer.util;
+
+/**
+ * @author houbinbin
+ * @since 16/9/15
+ *
+ * 参数工具类
+ */
+public class ArgUtil {
+
+  private ArgUtil(){}
+
+
+}
